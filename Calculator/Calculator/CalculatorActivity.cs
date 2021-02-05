6a039b9d412811cb1e9ff6fcbe5b987699eedfef
sink <59916608+sinkkkk@users.noreply.github.com>
2021-02-05 15:55:31 +0200

    Siim on lah

---
+++ Calculator/Calculator/CalculatorActivity.cs
@@ -0,0 +1,69 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Calculator
+{
+    [Activity(Label = "CalculatorActivity")]
+    public class CalculatorActivity : Activity
+    {
+        EditText firstEditText;
+        EditText secondEditText;
+        TextView answerText;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+            SetContentView(Resource.Layout.calculator_layout);
+
+            firstEditText = FindViewById<EditText>(Resource.Id.firstNumberEditText);
+            secondEditText = FindViewById<EditText>(Resource.Id.secondNumberEditText);
+            var addButton = FindViewById<Button>(Resource.Id.addButton);
+            var minbutton = FindViewById<Button>(Resource.Id.minbutton);
+            var korbutton = FindViewById<Button>(Resource.Id.korbutton);
+            var jagbutton = FindViewById<Button>(Resource.Id.jagbutton);
+            answerText = FindViewById<TextView>(Resource.Id.answerTextView);
+
+            addButton.Click += AddButton_Click;
+            minbutton.Click += minbutton_Click;
+            korbutton.Click += korbutton_Click;
+            jagbutton.Click += jagbutton_Click;
+
+
+        }
+
+        private void AddButton_Click(object sender, EventArgs e)
+        {
+            var answer = int.Parse(firstEditText.Text) + int.Parse(secondEditText.Text);
+            answerText.Text = answer.ToString();
+        }
+
+        private void minbutton_Click(object sender, EventArgs e)
+        {
+            var answer = int.Parse(firstEditText.Text) - int.Parse(secondEditText.Text);
+            answerText.Text = answer.ToString();
+        }
+
+        private void korbutton_Click(object sender, EventArgs e)
+        {
+            var answer = int.Parse(firstEditText.Text) * int.Parse(secondEditText.Text);
+            answerText.Text = answer.ToString();
+        }
+
+        private void jagbutton_Click(object sender, EventArgs e)
+        {
+            var answer = int.Parse(firstEditText.Text) / int.Parse(secondEditText.Text);
+            answerText.Text = answer.ToString();
+        }
+
+    }
+}
---
+++ Calculator/Calculator/MainActivity.cs
@@ -0,0 +1,36 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using Android.Content;
+
+namespace Calculator
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+            var button = FindViewById<Button>(Resource.Id.button1);
+            button.Click += Button_Click;
+        }
+
+        private void Button_Click(object sender, System.EventArgs e)
+        {
+            var intent = new Intent(this, typeof(CalculatorActivity));
+            StartActivity(intent);
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
